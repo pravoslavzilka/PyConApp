d27574b299370dc8b9356cd0431b29d156d0ff37
Pravoslav <pravoslav.zilka@gmail.com>
2022-07-21 00:02:12 +0200

    First commit

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def splash_screen():
+        return render_template("login_screen.html")
+
+
+@app.route("/get-login-info", methods=["POST"])
+def login_proceed():
+    # email = request.form["email-address"]
+    # password = request.form["password"]
+
+    return render_template("continue_login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
